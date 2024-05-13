3f1b19deff57ce7a9b801d4757e62346128dd7fa
MikolajMys <mikolaj.my@gmail.com>
2024-05-13 03:15:34 +0200

    app appearance update

--- react_todo_list/src/App.jsx
+++ react_todo_list/src/App.jsx
@@ -1,3 +1,30 @@
+import "./styles.css"
+
+
 export default function App() {
-  return "Hi"
+
+  return (
+    <>
+      <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Task</label>
+        <input 
+          type="text" 
+          id="item" 
+        />
+      </div>
+      <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
