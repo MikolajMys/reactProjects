19b37ed7f4ee75f9a8fc3987e71d05afbbbcb463
MikolajMys <mikolaj.my@gmail.com>
2024-05-13 03:40:29 +0200

    adding tasks update

--- react_todo_list/src/App.jsx
+++ react_todo_list/src/App.jsx
@@ -1,29 +1,54 @@
+import { useState } from "react"
 import "./styles.css"
 
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed:
+        false },
+      ]
+    })
+
+    setNewItem("")
+  }
 
   return (
     <>
-      <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Task</label>
-        <input 
-          type="text" 
-          id="item" 
-        />
-      </div>
-      <button className="btn">Add</button>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Task</label>
+          <input 
+            value={newItem} 
+            onChange={e => setNewItem(e.target.value)} 
+            type="text" 
+            id="item" 
+          />
+        </div>
+        <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input 
+                  type="checkbox" 
+                  checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}
       </ul>
     </>
   )
