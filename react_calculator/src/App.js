685a32f1f29a6249ddfa3397db8465979d495547
MikolajMys <mikolaj.my@gmail.com>
2024-06-04 13:51:34 +0200

    buttons update

--- react_calculator/src/App.js
+++ react_calculator/src/App.js
@@ -162,17 +162,17 @@ function App() {
       <button className="btn-top" onClick={() => dispatch({ type: ACTIONS.SIGN_CHANGE })}>+/-</button>
       <button className="btn-top" onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
-      <DigitButton digit="1" dispatch={dispatch} />
-      <DigitButton digit="2" dispatch={dispatch} />
-      <DigitButton digit="3" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
       <OperationButton operation="x" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
       <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="7" dispatch={dispatch} />
-      <DigitButton digit="8" dispatch={dispatch} />
-      <DigitButton digit="9" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
       <OperationButton operation="+" dispatch={dispatch} />
       <SpanTwoDigitButton digit="0" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
